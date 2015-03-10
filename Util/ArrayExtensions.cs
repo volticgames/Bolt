d098f316a109927648c7d37e4576277b5253e67d
Ben Follington <bfollington@gmail.com>
2015-03-10 11:44:25 +1000

    Added logging and array filling extensions to the project

---
+++ Util/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bolt {
+	public static class ArrayExtensions {
+		public static void Fill<T>(this T[] originalArray, T with) {
+			for(int i = 0; i < originalArray.Length; i++){
+				originalArray[i] = with;
+			}
+		}  
+		
+		public static void Fill<T>(this T[,] originalArray, T with) {
+			for(int i = 0; i < originalArray.GetLength(0); i++){
+				for (int j = 0; j < originalArray.GetLength(1); j++)
+				{
+					originalArray[i, j] = with;	
+				}
+			}
+		}  
+	}
+}
