e2ec9ebb3984a46252707f7215ac0e6d608054dd
Ben Follington <bfollington@gmail.com>
2015-08-13 14:25:31 +1000

    Helper methods and extensions for setting up new objects

---
+++ Util/New.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class New {
+
+	// Use this for initialization
+	public static GameObject GameObject (string name) {
+		var obj = Object.Instantiate( Resources.Load(name) as GameObject );
+		
+		return obj;
+	}
+	
+	public static GameObject GameObject (GameObject template) {
+		var obj = Object.Instantiate( template );
+		return obj;
+	}
+	
+	public static GameObject GameObject(string name, GameObject parent) {
+		var go = GameObject(name);
+		
+		go.transform.parent = parent.transform;
+		
+		return go;
+	}
+	
+	public static GameObject GameObject(string name, Transform parent) {
+		var go = GameObject(name);
+		
+		go.transform.parent = parent;
+		
+		return go;
+	}
+	
+	public static GameObject GameObject(GameObject template, GameObject parent) {
+		var go = GameObject(template);
+		
+		go.transform.parent = parent.transform;
+		
+		return go;
+	}
+	
+	public static GameObject GameObject(GameObject template, Transform parent) {
+		var go = GameObject(template);
+		
+		go.transform.parent = parent;
+		
+		return go;
+	}
+}
---
+++ Util/TransformExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Bolt {
+	public static class TransformExtensions {
+		public static void SetLocalPosition(this Transform transform, float x = 0, float y = 0, float z = 0) {
+			transform.localPosition = new Vector3(x, y, z);
+		}
+	}
+}
